39e63e82ee69fd216e2511095be6a85ec0487866
Eduardo Klein Diefenthaler <120498575+eduardo-klein-dev@users.noreply.github.com>
2022-12-14 16:00:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinemark</title>
+
+    <!-- Links CSS: -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Links Fonts: -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Archivo+Narrow:wght@700&display=swap');
+    </style>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="navbar">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <img class="logo" src="img/logo1.png" alt="Logo">
+                </a>
+                <div class="header-conteudo">
+                    <div class="header-conteudo-search">
+                        <form>
+                            <input type="text" name="search" placeholder="Search" />
+                            <button> <img src="img/loupe.png" alt="Lupa Pesquisa"></button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <section class="titulo text-center">
+            <div class="container">
+                <h1 class="titulo-heading">Filmes em Cartaz</h1>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row filmes">
+                    <div class="col-md-12"></div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer>
+        <div class="container">@Todos os direitos reservados - 2022</div>
+    </footer>
+
+
+    <!-- Links JS: -->
+    <script type="text/javascript" src="js/jquery-3.6.1.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js" defer></script>
+
+</body>
+
+</html>
